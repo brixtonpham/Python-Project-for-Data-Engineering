183fe281f276c573272e2c576fc3e6210948441e
Pham Thanh Nam <117471232+brixtonpham@users.noreply.github.com>
2024-03-19 14:20:51 +0700

    Complete Hands-on Lab: Extract, Transform, Load(ETL)

--- ExtractTransformLoad/LabETL/etl_code.py
+++ ExtractTransformLoad/LabETL/etl_code.py
@@ -29,15 +29,15 @@ def extract():
     extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data 
      
     # process all csv files 
-    for csvfile in glob.glob("*.csv"): 
+    for csvfile in glob.glob("./source/*.csv"): 
         extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True) 
          
     # process all json files 
-    for jsonfile in glob.glob("*.json"): 
+    for jsonfile in glob.glob("./source/*.json"): 
         extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True) 
      
     # process all xml files 
-    for xmlfile in glob.glob("*.xml"): 
+    for xmlfile in glob.glob("./source/*.xml"): 
         extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
          
     return extracted_data 
@@ -62,3 +62,32 @@ def log_progress(message):
     timestamp = now.strftime(timestamp_format) 
     with open(log_file,"a") as f: 
         f.write(timestamp + ',' + message + '\n') 
+
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+ 
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+ 
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+ 
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+ 
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+ 
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+ 
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+ 
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
